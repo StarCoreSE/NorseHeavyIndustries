b7c6ae7399bd13ac383b3ba3fdaed3e131057170
CommodoreChet <50426713+CommodoreChet@users.noreply.github.com>
2023-05-28 22:37:37 -0500

    Update NHI_Fixed_Autocannon.cs
    Max Heat to 1575, HPS to 37, Sink Rate to 135

--- Data/Scripts/Coreparts/NHI_Fixed/NHI_Fixed_Autocannon.cs
+++ Data/Scripts/Coreparts/NHI_Fixed/NHI_Fixed_Autocannon.cs
@@ -125,10 +125,10 @@ namespace Scripts
                     ReloadTime = 1, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
                     MagsToLoad = 1, // Number of physical magazines to consume on reload.
                     DelayUntilFire = 0, // How long the weapon waits before shooting after being told to fire. Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
-                    HeatPerShot = 50, // Heat generated per shot.
-                    MaxHeat = 1260, // Max heat before weapon enters cooldown (70% of max heat).
+                    HeatPerShot = 37, // Heat generated per shot.
+                    MaxHeat = 1575, // Max heat before weapon enters cooldown (70% of max heat).
                     Cooldown = .75f, // Percentage of max heat to be under to start firing again after overheat; accepts 0 - 0.95
-                    HeatSinkRate = 180, // Amount of heat lost per second.
+                    HeatSinkRate = 135, // Amount of heat lost per second.
                     DegradeRof = false, // Progressively lower rate of fire when over 80% heat threshold (80% of max heat).
                     ShotsInBurst = 3, // Use this if you don't want the weapon to fire an entire physical magazine in one go. Should not be more than your magazine capacity.
                     DelayAfterBurst = 0, // How long to spend "reloading" after each burst. Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
